10265ab4495257c3ddd01b051d40b3f507ba680c
juanrw <juan.rw24@gmail.com>
2024-05-29 14:49:34 -0500

    complete part 3 of lab

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+# Import the reverse function
+from django.urls import reverse
 
 # Create your models here.
 class Dog(models.Model):
@@ -9,3 +11,6 @@ class Dog(models.Model):
 
   def __str__(self):
     return self.name
+  
+  def get_absolute_url(self):
+    return reverse('dog-detail', kwargs={'dog_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Dog
 # Add the following import
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 # Create your views here.
@@ -20,3 +21,16 @@ def dog_index(request):
 def dog_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   return render(request, 'dogs/detail.html', { 'dog': dog })
+
+class DogCreate(CreateView):
+  model = Dog
+  fields= '__all__'
+
+class DogUpdate(UpdateView):
+  model = Dog
+  # Let's disallow the renaming of a Dog by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
