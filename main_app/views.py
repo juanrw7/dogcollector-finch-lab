9476fc3fb4de69a3cc89e55af61a38f1a2b9a901
juanrw <juan.rw24@gmail.com>
2024-05-29 10:29:54 -0500

    complete part 2 of lab

--- main_app/views.py
+++ main_app/views.py
@@ -1,30 +1,22 @@
 from django.shortcuts import render
+from .models import Dog
 # Add the following import
 from django.http import HttpResponse
 
-# Add the Cat class & list and view function below the imports
-class Dog:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Enzo', 'Border Collie', 'A gentleman.', 8),
-  Dog('Hela', 'Dachshund', 'Crazy but adorable.', 1),
-  Dog('Nicky', 'Chihuahua', 'Shy but likes to bark.', 1),
-  Dog('Milo', 'Chihuahua', 'Helas minion.', 0)
-]
 
 # Create your views here.
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def dog_index(request):
+  dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+def dog_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', { 'dog': dog })
